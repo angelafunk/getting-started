f1f01a0154a07ad105f76e71fc6130eec896cb84
Angela <funka@google.com>
2017-11-14 09:37:08 -0800

    Initializing step

---
+++ main.go
@@ -0,0 +1,15 @@
+
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func init() {
+    http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "The Gopher Network")
+}
